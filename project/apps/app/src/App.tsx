4888ef12bc5360493c8cddfe31be1941eafd873e
Mahalakshmi Vallepu <mahalakshmi.vallepu@morganlewis.com>
2024-12-16 19:54:52 +0530

    task-2(showing data in list)

--- project/apps/app/src/App.tsx
+++ project/apps/app/src/App.tsx
@@ -2,6 +2,35 @@ import { List } from 'ui';
 import React, { useState, useEffect } from 'react';
 const api = "https://pokeapi.co/api/v2/pokemon?limit=151"
 
+interface Pokemon {
+  name: string;
+  url: string;
+}
+
+interface PokemonListProps {
+  pokemon: Pokemon[];
+}
+
+const PokemonList = ({ pokemon }: PokemonListProps) => {
+  if (!pokemon.length) {
+    return <p>No pokemons found.</p>;
+  }
+
+  return (
+    <ul>
+      {pokemon.map((pokemon, index) => (
+        <li key={index}>
+          <h2>{pokemon.name}</h2>
+          <p>ID: {index + 1}</p>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+
+
+
 const App = () => {
   const [pokemon, setPokemon] = useState([]);
   const [loading, setLoading] = useState(true);
@@ -26,10 +55,10 @@ const App = () => {
     fetchData();
   }, []);
   return (
-  <>
-    <h1>Pokemon list:</h1>
-    <List />
-  </>
+ <>
+      <h1>Pokemon list:</h1>
+      {loading ? <p>Loading...</p> : <PokemonList pokemon={pokemon} />}
+    </>
   )
 }
 
