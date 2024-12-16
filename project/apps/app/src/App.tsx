291b5ee2b4c14f63e306decd0b0d45366e43a309
Mahalakshmi Vallepu <mahalakshmi.vallepu@morganlewis.com>
2024-12-16 19:16:01 +0530

    task-1 (Using fetch method getting the data from API and also we can use axiox method to get data)

--- project/apps/app/src/App.tsx
+++ project/apps/app/src/App.tsx
@@ -1,8 +1,30 @@
-import { List } from 'ui'
-
+import { List } from 'ui';
+import React, { useState, useEffect } from 'react';
 const api = "https://pokeapi.co/api/v2/pokemon?limit=151"
 
 const App = () => {
+  const [pokemon, setPokemon] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(api);
+        if (!response.ok) {
+          throw new Error(response.statusText);
+        }
+        const data = await response.json();
+        console.log('data..',data)
+        setPokemon(data.results.map((pokemon: any) => ({ name: pokemon.name, url: pokemon.url })));
+        setLoading(false);
+      } catch (error) {
+        //setError(error.message);
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
   return (
   <>
     <h1>Pokemon list:</h1>
